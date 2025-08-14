da17cc017d231eaa9ebca861598b19a157a71b1c
Candela Zapata <candezapata2011@hotmail.com>
2025-08-13 22:36:05 -0300

    correcion de conexion de bd

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/escuela")
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("✅ Conexión exitosa a MongoDB")
   } catch (error) {
     console.error("❌ Error al conectar a MongoDB:", error.message)
